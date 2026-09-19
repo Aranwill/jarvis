8df6c4c685571baa50d73928ecf8a88ce812613d
Hector Rodriguez <80172798+Aranwill@users.noreply.github.com>
2026-09-19 08:48:33 -0300

    feat(e1): add governed knowledge package

---
+++ src/malak/knowledge/__init__.py
@@ -0,0 +1,17 @@
+"""Governed knowledge read primitives for Malāk."""
+
+from malak.knowledge.knowledge_reader import (
+    GovernedKnowledgeReader,
+    KnowledgeDocument,
+    KnowledgeSearchResult,
+    KnowledgeSource,
+    KnowledgeTextMatch,
+)
+
+__all__ = [
+    "GovernedKnowledgeReader",
+    "KnowledgeDocument",
+    "KnowledgeSearchResult",
+    "KnowledgeSource",
+    "KnowledgeTextMatch",
+]
